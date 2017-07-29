9e2fc109f83d4077d7aa802edaa40366c228d1df
Mathias Rav <rav@cs.au.dk>
2017-07-29 11:23:27 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = '''\
+Serve a single file download via HTTP
+'''.rstrip()
+
+CONSOLE_SCRIPTS = [
+    'singlehttpd = singlehttpd:main',
+]
+
+
+headline = DESCRIPTION.split('\n', 1)[0].rstrip('.')
+
+
+setup(
+    name='singlehttpd',
+    version='0.1',
+    description=headline,
+    long_description=DESCRIPTION,
+    author='https://github.com/Mortal',
+    url='https://github.com/Mortal/singlehttpd',
+    py_modules=['singlehttpd'],
+    include_package_data=True,
+    license='GPLv3',
+    entry_points={
+        'console_scripts': CONSOLE_SCRIPTS,
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ singlehttpd.py
@@ -0,0 +1,41 @@
+import os
+import asyncio
+import argparse
+import functools
+from aiohttp import web
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--listen-host', default='0.0.0.0')
+parser.add_argument('-p', '--listen-port', default=8080, type=int)
+parser.add_argument('path')
+
+
+async def handler(args, request):
+    filename = os.path.basename(args.path)
+    headers = {'Content-Disposition': 'attachment; filename=' + filename}
+    return web.FileResponse(args.path, headers=headers)
+
+
+async def run_server(loop, args):
+    server = web.Server(functools.partial(handler, args))
+    await loop.create_server(server, args.listen_host, args.listen_port)
+    print("======= Serving on http://%s:%s/ ======" %
+          (args.listen_host, args.listen_port))
+    while True:
+        await asyncio.sleep(100*3600)
+
+
+def main():
+    args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(run_server(loop, args))
+    except KeyboardInterrupt:
+        pass
+    loop.close()
+
+
+if __name__ == '__main__':
+    main()
